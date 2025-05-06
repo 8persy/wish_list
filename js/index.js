a644bf380200c6e86076ca6060acaf73bb2cf1af
Almaz <almaz.nasrutdinov@gmail.com>
2025-05-06 21:11:45 +0300

    add popup js, localstorage, card functions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+
+document.getElementById('imageUpload').addEventListener('change', function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById('imagePreview').src = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+document.getElementById('tagsBtn').addEventListener('click', function (e) {
+  e.stopPropagation();
+  const dropdown = document.getElementById('tagsDropdown');
+  dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
+});
+
+function closePopup() {
+  document.getElementById('editPopup').style.display = 'none';
+  document.querySelector('.overlay').style.display = 'none'
+}
 document.addEventListener('DOMContentLoaded', () => {
   const cardContainer = document.querySelector('.cards');
   const cardTemplate = document.querySelector('#card-template');
