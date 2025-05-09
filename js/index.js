05839e987e7ba5aaaff7aed5da5ed4bb04ee2b97
8percy8 <izergin.v05@mail.ru>
2025-05-09 17:18:47 +0300

    add modal closing

--- js/index.js
+++ js/index.js
@@ -1,25 +1,3 @@
-document.getElementById('imageUpload').addEventListener('change', function () {
-    const file = this.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            document.getElementById('imagePreview').src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-    }
-});
-
-document.getElementById('tagsBtn').addEventListener('click', function (e) {
-    e.stopPropagation();
-    const dropdown = document.getElementById('tagsDropdown');
-    dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
-});
-
-function closePopup() {
-    document.getElementById('editPopup').style.display = 'none';
-    document.querySelector('.overlay').style.display = 'none'
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const cardContainer = document.querySelector('.cards');
     const cardTemplate = document.querySelector('#card-template');
@@ -64,6 +42,23 @@ document.addEventListener('DOMContentLoaded', () => {
         let end = getWishWord(count)
         wishCount.textContent = `Найдено ${count} ${end}`
     }
+
+    document.getElementById('imageUpload').addEventListener('change', function () {
+        const file = this.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById('imagePreview').src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    document.getElementById('tagsBtn').addEventListener('click', function (e) {
+        e.stopPropagation();
+        const dropdown = document.getElementById('tagsDropdown');
+        dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
+    });
 
     function openPopup() {
         editPopup.classList.remove('hidden');
